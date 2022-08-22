0bf3b170f7734144a604dfe02145ec560dfb2354
Suanig <suanig88@gmail.com>
2022-08-22 19:12:47 +0300

    add HWL1T4 Even odd from 1 to N

---
+++ HWL1T4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число N ");
+double numberN = double.Parse(Console.ReadLine());
+double count = 0;
+if (numberN > 1) 
+    {
+    for (count = 2; count < numberN; count = count + 2)
+    {
+      Console.WriteLine(count);   
+    }
+    }
+    else 
+    {
+        if (numberN < -1)
+        {
+           for (count = -2; count > numberN; count = count - 2)
+    {
+      Console.WriteLine(count);   
+    }
+        }
+        else Console.Write("None");
+    }

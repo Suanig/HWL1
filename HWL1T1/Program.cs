1789713a1914eb3df0082be3f8236d2a96c89922
Suanig <suanig88@gmail.com>
2022-08-22 18:32:24 +0300

    add HWL1T1 Max of 2

---
+++ HWL1T1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число А ");
+double numberA = double.Parse(Console.ReadLine());
+Console.Write("Введите число B ");
+double numberB = double.Parse(Console.ReadLine());
+double max = numberA;
+if (numberA >= numberB)
+{
+    max = numberA;
+}
+else
+{
+    max = numberB;
+}
+Console.Write("Max= ");
+Console.Write(max);

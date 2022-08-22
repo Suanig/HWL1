c86bfcc712be7c31160df22c68bc8606f7420c0e
Suanig <suanig88@gmail.com>
2022-08-22 18:48:43 +0300

    add HWL1T3 Even odd

---
+++ HWL1T3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число А ");
+double numberA = double.Parse(Console.ReadLine());
+if (numberA % 2 == 0)
+{
+    Console.Write("Число А - четное");
+}
+else 
+{
+    Console.Write("Число А - Нечетное");
+}
